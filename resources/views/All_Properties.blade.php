335759a690082330496d3b8e314368295cde8674
hicham elquagh <alquaghhicham1@gmail.com>
2023-04-12 11:06:12 +0000

    i added filter to the appartemetns just front-end

--- resources/views/All_Properties.blade.php
+++ resources/views/All_Properties.blade.php
@@ -3,20 +3,67 @@
     @include('nav')
   
    <div class=" bg-properties">
-    <div class="  container"> 
-                  <div class="d-flex justify-content-start "> 
-                    <div> <p class="properties">list of properties</p></div>
-                   <!-- <div class="d-flex algin-items-center"><button class="btn-view">View All Properties</button></div> -->
-                  </div>
+     <div class="d-flex justify-content-start "> 
+       <div> <p class="properties">list of properties</p></div>
+     </div>
+    <div class=" my-5"> 
+     
 
 
 
-       <div class="row mx-auto ">
+
+
+       
+    <div class="row mx-auto ">
+      <div class="col-lg-3 col-md-12 mb-lg-0 mb-4 px-0">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
+            <div class="container-fluid flex-lg-column align-items-stretch">
+                <h4 class="mt-2">FILTERS</h4>
+                <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="filterDropdown">
+                    <div class="border bg-light p-3 rounded mb-3">
+                        <h5 class="mb-3" style="font-size: 18px;">CHECK AVAILABILITY</h5>
+                        <label class="form-label">Check-in</label>
+                        <input type="date" class="form-control shadow-none mb-3">
+                        <label class="form-label">Check-in</label>
+                        <input type="date" class="form-control shadow-none">
+                    </div>
+                    <div class="border bg-light p-3 rounded mb-3">
+                        <h5 class="mb-3" style="font-size: 18px;">FACILITIES</h5>
+                        <div class="mb-2">
+                            <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
+                            <label class="form-check-label" for="f1">WIFI</label>
+                        </div>
+                        <div class="mb-2">
+                            <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
+                            <label class="form-check-label" for="f2">TV</label>
+                        </div>
+                        <div class="mb-2">
+                            <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
+                            <label class="form-check-label" for="f3">AC</label>
+                        </div>
+                    </div>
+                     <div class="border bg-light p-3 rounded mb-3">
+                        <h5 class="mb-3" style="font-size: 18px;">Adults</h5>
+                        <div class="d-flex">
+                            <div class="me-2">
+                                <label class="form-label">Adults</label>
+                                <input type="number" class="form-control shadow-none">
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </nav></div>
+      <div class="row col-lg-9 col-md-12 mb-lg-0">
           @foreach($appartements as $appartement)
-      <div class="col-md-6 col-lg-4 p-2 col-12" href="#modal-meal" data-bs-toggle="modal" >
+        <div class="col-md-6 col-lg-4 p-2 col-12" href="#modal-meal" data-bs-toggle="modal" >
             <div class="card border-0 rounded-4 bg-light  shadow-lg  rounded">
               @foreach ($appartement->images as $image)
             <a href="properties"><img src="{{asset('storage/image/'.$image->image)}}" class="card-img-top" > </a> 
+            @break
             @endforeach;
             <div class="card-body text-center">
               <h5 class="card-title fw-bold text-dark">{{$appartement->name}}</h5>
@@ -26,11 +73,11 @@
               <p class="fw-bold text-dark">Status : {{$appartement->status}}</p>
               <p class="text-dark"><span class="fw-bold text-dark">Date :</span>{{$appartement->date}}</p>
             </div>    
-            @endforeach;
           </div>
         </div> 
-
-
+        @endforeach</div>
+          
+      
        
       
 
@@ -39,7 +86,7 @@
           
      </div>
   </div>
-  </div></a> 
+</a> 
     
 
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,71 +1,47 @@
 
- <!-- modal -->
- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content ">
-        <div class=" modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Nom de l'appartement</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <form action="{{route('appartement.store')}}" method="POST" id="form" enctype="multipart/form-data">
-              @csrf
-                
-              <label for="recipient-name" class="col-form-label" >choisir votre Ville</label>
-          <select class="form-select" aria-label="Default select example" name="city">
-              @foreach($cities as $city)
-                <option value="{{$city->id}}">{{$city->name}}</option>  
-                @endforeach;  
-          </select>
-          
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Adresse</label>
-              <input type="text" class="form-control" name="localisation" id="recipient-name">
-            </div>
-            <div class="mb-3">
-              <label for="message-text" class="col-form-label">Description </label>
-              <textarea class="form-control" name="description" id="message-text"></textarea>
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Image</label>
-              <input type="file" class="form-control" name="images[]" multiple id="recipient-name">
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Prix</label>
-              <input type="number" class="form-control" name="prix" id="recipient-name" step="0.01" min="0">
-            </div>
-           
-          <label for="recipient-name" class="col-form-label" for="tags">Sélectionnez des Caracteristique:</label>
-              <select class="form-select" aria-label="Default select example" name="caracteristique[]" id="tags" multiple>
-                  @foreach($characteristics as $characteristic)
-                  <option value="{{$characteristic->id}}">{{$characteristic->name}}</option>  
-                  @endforeach; 
-              </select>
-              <div class="mb-3">
-                  <label for="recipient-name" class="col-form-label">Nombre de Chambre</label>
-                  <input type="number" class="form-control" name="nombreChambre" id="recipient-name">
-                </div>
-              <label for="recipient-name" class="col-form-label" >Nombre de Personne</label>
-          <select class="form-select" aria-label="Default select example" name="nombrePersonne">
-              <option selected>Nombre de Personne</option>
-              @foreach($persons as $person)
-              <option value="{{$person->id}}">{{$person->person}}</option>  
-              @endforeach;  
-          </select>
-                  <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Espaces</label>
-              <input type="text" class="form-control" name="espaces" id="recipient-name">
-            </div>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label">Date</label>
-              <input type="date" class="form-control" name="date" id="recipient-name">
-            </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary" name="save" >Save</button>
-        </div>
-        </form>
+
+@include('header');
+<div class="col-lg-3 col-md-12 mb-lg-0 mb-4 px-0">
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-white rounded shadow">
+      <div class="container-fluid flex-lg-column align-items-stretch">
+          <h4 class="mt-2">FILTERS</h4>
+          <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="filterDropdown">
+              <div class="border bg-light p-3 rounded mb-3">
+                  <h5 class="mb-3" style="font-size: 18px;">CHECK AVAILABILITY</h5>
+                  <label class="form-label">Check-in</label>
+                  <input type="date" class="form-control shadow-none mb-3">
+                  <label class="form-label">Check-in</label>
+                  <input type="date" class="form-control shadow-none">
+              </div>
+              <div class="border bg-light p-3 rounded mb-3">
+                  <h5 class="mb-3" style="font-size: 18px;">FACILITIES</h5>
+                  <div class="mb-2">
+                      <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
+                      <label class="form-check-label" for="f1">WIFI</label>
+                  </div>
+                  <div class="mb-2">
+                      <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
+                      <label class="form-check-label" for="f2">TV</label>
+                  </div>
+                  <div class="mb-2">
+                      <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
+                      <label class="form-check-label" for="f3">AC</label>
+                  </div>
+              </div>
+               <div class="border bg-light p-3 rounded mb-3">
+                  <h5 class="mb-3" style="font-size: 18px;">Adults</h5>
+                  <div class="d-flex">
+                      <div class="me-2">
+                          <label class="form-label">Adults</label>
+                          <input type="number" class="form-control shadow-none">
+                      </div>
+                  </div>
+              </div> 
+          </div>
       </div>
-    </div>
-  </div>
+  </nav>
+</div>
